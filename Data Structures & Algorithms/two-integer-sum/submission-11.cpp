04bb882b879d1b15f307185a9a1dbf4418414210
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2026-06-13 09:55:48 +0200

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> prev;
+
+        for (int i = 0; i < nums.size();i++) {
+            int diff = target - nums[i];
+            if (prev.count(diff)) {
+                return {prev[diff], i};
+            } 
+            prev[nums[i]] = i;
+        }
+        return {};
+    }
+};
